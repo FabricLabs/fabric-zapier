5abefddf3e62171f70f5f9ff4f20a0da59d287ef
Eric Martindale <eric@ericmartindale.com>
2021-10-20 03:17:27 -0400

    Leverage new Remote semantics

--- services/zapier.js
+++ services/zapier.js
@@ -12,6 +12,8 @@ class Zapier extends Service {
       hooks: {}
     }, settings);
 
+    this.remote = null;
+
     this._state = {
       status: 'STOPPED',
       hooks: {}
@@ -25,26 +27,26 @@ class Zapier extends Service {
   }
 
   trust (source) {
-    super.trust(source);
-
-    return {
+    const listeners = super.trust(source);
+    return merge({}, listeners, {
       _handleTriggerEvent: source.on('trigger', this._handleTrustedTrigger.bind(this))
-    };
+    });
   }
 
   async notify (name, message = 'Default Zapier event!  Provide a `message` parameter to override.') {
     this.emit('log', `[ZAPIER] Triggering: ${name} :: [ ${message} ]`);
     if (!this.hooks[name]) return this.emit('error', `Hook does not exist: ${name}`);
-    return this.trigger(name, [message]);
+    return this.trigger(name, {
+      content: message
+    });
   }
 
   async start () {
     this.remote = new Remote({ authority: 'hooks.zapier.com' });
+
     for (const [name, address] of Object.entries(this.settings.hooks)) {
       await this._registerHook(name, address);
     }
-
-    await this.notify('contact', `Zapier service started.  Agent ID: ${this.id}`);
 
     this.emit('ready', {
       id: this.id
@@ -53,10 +55,14 @@ class Zapier extends Service {
     return this;
   }
 
-  async trigger (name, params = []) {
-    return this.remote._POST(this.hooks[name], {
-      content: params
+  async trigger (name, data = null) {
+    const result = this.remote._POST(this.hooks[name], data, { mode: 'query' });
+    this.emit('trigger', {
+      name: name,
+      data: data,
+      result: result
     });
+    return result;
   }
 
   async _handleTrustedTrigger (trigger) {
